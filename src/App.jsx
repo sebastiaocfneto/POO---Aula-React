495b850b04f8ab53fcacca99928cb6770bc8701f
sebastiaocfneto <147873685+sebastiaocfneto@users.noreply.github.com>
2024-06-24 18:34:53 +0000

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { Header } from "./components/Header"
+function App() {
+
+  //const [count, setCount] = useState(0)
+
+  return (
+    <div>
+      <Header title={"POO"}/>
+      <Header title={"E-comerce"}/>
+      <Header/>
+      <Header/>
+      <Header/>
+      <Header/>
+    </div>
+  )
+}
+
+export default App
